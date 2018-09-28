4248839cef624f2b408746d41a17443b7e00fe63
Wyrd00 <amy.tai0120@gmail.com>
2018-09-28 16:43:07 -0700

    revert codes back to non-debugging mode, deleted home.html

--- JDID/helper_methods.py
+++ JDID/helper_methods.py
@@ -5,13 +5,11 @@ from datetime import datetime, date
 def is_goal_editable(goal_obj):
     """return True if goal is longer than 5 days and less than a quarter towards deadline"""
     today = date.today()
-    # try:
-    duration = (goal_obj.deadline - goal_obj.start_date).days
-    editable_period = (duration//4)
-    if (duration > 5 and (today - goal_obj.start_date).days < editable_period):
-        return True
-    else:
+    try:
+        duration = (goal_obj.deadline - goal_obj.start_date).days
+        editable_period = (duration//4)
+        if (duration > 5 and (today - goal_obj.start_date).days < editable_period):
+            return True
+    except:
         return False
-    # except Exception:
-    #     return False
-    # return False
+    return False
