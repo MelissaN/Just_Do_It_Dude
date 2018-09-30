b21ca1461a6736dd9e14de59477dd6c925753062
MelissaN <majajachiu@yahoo.com>
2018-09-30 01:44:13 +0000

    move email method into helper_methods file

--- JDID/helper_methods.py
+++ JDID/helper_methods.py
@@ -13,3 +13,11 @@ def is_goal_editable(goal_obj):
     except:
         return False
     return False
+
+
+def email_accountability_partner():
+    """send email to accountability partner to inform them of new goal submission"""
+    msg = Message('Hello from Just Do It Dude!', sender=(
+        os.environ.get('MY_EMAIL')), recipients=[partner_email])
+    msg.body = "Dear " + accountability_partner + ", Woohoo! Starting now, your friend has a goal to " + goal + " by " + deadline + ". Even cooler, they've asked that you hold them accountable. If they don't succeed in accomplishing their goal by their deadline, in their own words they've pledged to '" + pledge + "!'"
+    mail.send(msg)
