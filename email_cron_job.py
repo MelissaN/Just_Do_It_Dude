e839d22d8bfc3703291b10d0ab95b8fc386a484d
Wyrd00 <amy.tai0120@gmail.com>
2018-10-04 18:02:41 -0700

    changed more hardcoded values in cron job

--- email_cron_job.py
+++ email_cron_job.py
@@ -8,13 +8,11 @@ from JDID import helper_methods
 from JDID.classes import storage
 import os
 
-#app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
@@ -25,14 +23,13 @@ with app.app_context(), app.test_request_context(base_url='http://35.221.52.120'
        Check daily if today is deadline date for any user's goal
        Email accountability partner and have them confirm if goal accomplished
     """
-    #TODO: change back recipient email to partner email after testing
     today = str(date.today())
     for rec in storage.all().values():
         if str(rec.deadline) == today:
             user_obj = storage.get_user_by_id(rec.user_id)
             subject = "It's time to check up on your friend!"
             sender = os.environ.get('MAIL_USERNAME')
-            recipients = os.environ.get('MAIL_USERNAME')
+            recipients = rec.partner_email
             cc = [user_obj.email]
             helper_methods.email_goal_confirmation(subject, sender, recipients, render_template("email_txt_template.txt", 
                             user=user_obj, goal=rec), render_template("email_template.html", 
